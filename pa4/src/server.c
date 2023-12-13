6e71fd545eb7683a6901cd6120d6f68c55e4969a
keati090 <146777539+keati090@users.noreply.github.com>
2023-12-13 17:43:12 -0600

    Update server.c

--- pa4/src/server.c
+++ pa4/src/server.c
@@ -7,16 +7,74 @@
 
 
 void *clientHandler(void *socket) {
+    processing_args_t *pargs = (processing_args_t *) socket;
+    printf("[SERVER]: client handler\n");
 
-    // Receive packets from the client
+    while (1) {
+        // Receive packets from the client
+        char data[BUFF_SIZE];
+        memset(data, 0, BUFF_SIZE);
+        recv(pargs->number_worker, data, BUFF_SIZE, 0);
+        packet_t *packet = deserializeData(data);
 
-    // Determine the packet operatation and flags
+        // Determine the packet operation and flags
+        if (packet->operation == IMG_OP_EXIT) {
+            free(packet);
+            close(socket);
+            pthread_exit(NULL);
+        }
 
-    // Receive the image data using the size
+        // Receive the image data using the size
 
-    // Process the image data based on the set of flags
+        // Process the image data based on the set of flags
+        if (packet->operation == IMG_OP_ROTATE) {
+            printf("%s\n", pargs->file_name);
+            int width;
+            int height;
+            int bpp;
 
-    // Acknowledge the request and return the processed image data
+            uint8_t* image_result = stbi_load(socket->filename, &width, &height, &bpp, CHANNEL_NUM);
+
+            uint8_t** img_matrix = (uint8_t **) malloc(sizeof(uint8_t *) * width);
+            uint8_t** result_matrix = (uint8_t **) malloc(sizeof(uint8_t *) * width);
+            for (int i = 0; i < width; i++) {
+                img_matrix[i] = (uint8_t *) malloc(sizeof(uint8_t) * height);
+                result_matrix[i] = (uint8_t *) malloc(sizeof(uint8_t) * height);
+            }
+    
+            linear_to_image(image_result, img_matrix, width, height);
+
+            if (packet->flags == IMG_FLAG_ROTATE_180) { // need to get angle from struct
+                flip_left_to_right(img_matrix, result_matrix, width, height);
+            } else if (packet->flags == IMG_FLAG_ROTATE_270) {
+                flip_upside_down(img_matrix, result_matrix, width, height);
+            }
+    
+            uint8_t* img_array = (uint8_t *) malloc(sizeof(uint8_t) * width * height);
+            flatten_mat(result_matrix, img_array, width, height);
+
+            if (stbi_write_png(out, width, height, CHANNEL_NUM, img_array, width*CHANNEL_NUM) == 0) {
+                exit(-1);
+            }
+
+            memset(buf, 0, 1024);
+            memset(out, 0, 1024);
+
+            for (int i = 0; i < width; i++) {
+                free(img_matrix[i]);
+                free(result_matrix[i]);
+            }
+
+            free(image_result);
+            free(img_matrix);
+            free(result_matrix);
+            free(img_array);
+        }
+
+        // Acknowledge the request and return the processed image data
+
+        free(packet);
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -50,6 +108,13 @@ int main(int argc, char* argv[]) {
     if(conn_fd == -1){
         perror("accept error");
     }
+
+    /*pthread_t proc_thread;
+    pthread_create(&proc_thread, NULL, (void *)clientHandler, (void *)&conn_fd);
+    printf("[SERVER]: thread created for client\n");
+
+    pthread_join(proc_thread, NULL);*/
+    
     // For intermediate, just accept one package then exit
     packet_t package;
     ret = recv(conn_fd, &package, sizeof(packet_t), 0);
