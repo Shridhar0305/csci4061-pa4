7a01e4f4cde8032db80a5e660700e19c8654e3fc
keati090 <146777539+keati090@users.noreply.github.com>
2023-12-13 20:50:41 -0600

    Create common.h

---
+++ pa4/include/common.h
@@ -0,0 +1,44 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PORT 9637
+#define STRSZ 1024
+
+// Protocol operations
+#define PROTO_REV 0
+#define PROTO_ACK 1
+
+// Packet structure
+struct Packet_Structure {
+    int operation;
+    char data[STRSZ];
+};
+
+typedef struct Packet_Structure Packet;
+
+const int PACKETSZ = sizeof(packet_t);
+
+// serialize packet
+char *serializePacket(Packet *packet){
+    char *serializedData = (char *)malloc(sizeof(char) * PACKETSZ);
+    memset(serializedData, 0, PACKETSZ);
+    memcpy(serializedData, packet, PACKETSZ);
+    return serializedData;
+}
+
+// deserialize data
+packet_t *deserializeData(char *serializedData){
+    packet_t *packet = (Packet *)malloc(sizeof(Packet));
+    memset(packet, 0, PACKETSZ);
+    memcpy(packet, serializedData, PACKETSZ);
+    return packet;
+}
+
+#endif
